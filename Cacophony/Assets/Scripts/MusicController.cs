ea6e033d4c0b7d30fcd2015f495c894236b68e11
Gabriel R <gabriel_ribeiro@id.uff.br>
2020-09-02 02:31:17 -0300

    First version of the Music Controller - Changes the object's state according to the music beat

--- Cacophony/Assets/Scripts/MusicController.cs
+++ Cacophony/Assets/Scripts/MusicController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip music;
+    public int musicBPM;
+
+    AudioClip currentMusic;
+
+    private int currentBPM;
+    private float currentBPS;
+    private float currentSPB;
+    private float currentStartTime;
+
+    private bool tempoMarker;
+
+    public MusicObject musicObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpNewMusic(music, musicBPM);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetUpNewMusic(AudioClip newMusic, int bpm)
     {
-        
+        currentBPM = bpm;
+        currentBPS = currentBPM / 60f;
+        currentSPB = 1f / currentBPS;
+        audioSource.clip = newMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+        currentStartTime = Time.time;
+        StopAllCoroutines();
+        StartCoroutine(WaitUntilNextBeat());
+    }
+
+    private IEnumerator WaitUntilNextBeat()
+    {
+        yield return new WaitForSeconds(currentSPB);
+        musicObject.Change();
+        StartCoroutine(WaitUntilNextBeat());
+    }
+
+    public bool GetTempoMarker()
+    {
+        return tempoMarker;
     }
 }
--- Cacophony/Assets/Scripts/MusicObject.cs
+++ Cacophony/Assets/Scripts/MusicObject.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class MusicObject : MonoBehaviour
 {
+    public List<GameObject> objects;
+    MusicController musicController;
+
+    private int currentIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicController = FindObjectOfType<MusicController>();
+
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].SetActive(false);
+        }
+        currentIndex = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Change()
     {
-        
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].SetActive(false);
+        }
+        objects[currentIndex].SetActive(true);
+
+        currentIndex++;
+
+        if(currentIndex == objects.Count)
+        {
+            currentIndex = 0;
+        }
     }
 }
